485e99026e40545fb9142bfc05e94bcb9fb34653
Rohit <vr.rohit03@gmail.com>
2021-07-02 20:50:53 +0530

    Mongoose connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "<url>";
+const PORT = process.env.PORT || 5000;
+
+/* setting up mongoose connection */
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch((err) => console.log(err.message));
+
+mongoose.set('useFindAndModify', false);
