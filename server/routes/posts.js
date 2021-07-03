f020afc9bbdd8881b1564b79fd80dbcc6cc33f9e
Rohit <vr.rohit03@gmail.com>
2021-07-03 10:30:53 +0530

    Added controllers and models

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,11 @@
 import express from "express";
 
+import { getPosts } from "../controllers/posts.js";
+
 const router = express.Router();
 
 // route: '/posts'
 
-router.get('/', (req, res) => {
-    res.send("Working!");
-});
+router.get('/', getPosts);
 
 export default router;
