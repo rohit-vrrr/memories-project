4c3f048eba9c43d1c77ebe625db0ee47234c9202
Rohit <vr.rohit03@gmail.com>
2021-07-03 13:33:54 +0530

    Updated with styles

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import useStyles from "./stylesForm";
+
+const Form = () => {
+    const classes = useStyles();
+
+    return (
+        <h1>FORM</h1>
+    );
+}
+
+export default Form;
