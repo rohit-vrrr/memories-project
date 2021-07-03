a683d4b87b271c287cb14c2407c5c6c2f6b97c59
Rohit <vr.rohit03@gmail.com>
2021-07-03 12:19:00 +0530

    server routes handling

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,25 @@
-export const getPosts = (req, res) => {
-    res.send("Working!");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch(err) {
+        res.status(404).json({ message: err.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch(err) {
+        res.status(409).json({ message: err.message });
+    }
 }
