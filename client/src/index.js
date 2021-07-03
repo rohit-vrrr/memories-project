4b8280ec560400cd66cdf23ff8163f01631c0ae3
Rohit <vr.rohit03@gmail.com>
2021-07-03 18:30:26 +0530

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "react-thunk";
 
 import App from "./App.jsx";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// setting up redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
