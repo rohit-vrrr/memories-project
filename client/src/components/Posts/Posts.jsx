b2001d1a0be5a3e52fcbaf1986fcd35b25972954
Rohit <vr.rohit03@gmail.com>
2021-07-06 14:43:45 +0530

    Display cards post

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Post from "./Post/Post";
-// import useStyles from "./stylesPosts";
+import useStyles from "./stylesPosts";
 
 const Posts = () => {
-    // const classes = useStyles();
+    const classes = useStyles();
     const posts = useSelector((state) => state.posts);
 
     console.log(posts);
     
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 
