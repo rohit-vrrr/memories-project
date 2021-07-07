da7cc26b9ba1cb5c874d28acc1b7dc3ec187213b
Rohit <vr.rohit03@gmail.com>
2021-07-07 17:26:59 +0530

    env variables

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -13,11 +15,10 @@ app.use(cors());
 // prefix the postRoutes with '/posts'
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "<url>";
 const PORT = process.env.PORT || 5000;
 
 /* setting up mongoose connection */
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
