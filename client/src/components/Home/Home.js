00d60d17ae6111ca4cfe8d8dd361092889d6c940
Rohit <vr.rohit03@gmail.com>
2021-07-15 12:55:46 +0530

    Copyrights

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
+import "./homeStyles.css";
 import { getPosts } from "../../actions/posts";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
@@ -16,20 +17,23 @@ const Home = () => {
     }, [currentId, dispatch]);
 
     return (
-        <Grow in>
-            <Container>
+        <div>
+            <Grow in>
+                <Container>
 
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId} />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    </Grid>
-                </Grid>
-                
-            </Container>
-        </Grow>
+                    
+                </Container>
+            </Grow>
+            <span className="copyright">Rohit © {new Date().getFullYear()}</span>
+        </div>
     );
 }
 export default Home;
